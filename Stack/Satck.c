fa7f6b444c970201aaa35a1e2e0fd997ca29ce21
Yash Kashyap <kashyapyash592@gmail.com>
2025-11-08 16:16:47 +0530

    Add files via upload

---
+++ Stack/Satck.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define SIZE 100
+
+int stack[SIZE];
+int top = -1;
+
+void push(int val){
+    if(top == SIZE - 1)
+        printf("Stack Overflow\n");
+    else
+        stack[++top] = value;
+}
+
+void pop(){
+    if (top == -1)
+    printf("Stack Underflow"\n);
+    else
+    printf(Popped : $d\n),stack[top--];
+    
+}
+void peek(){
+    if(top == -1)
+        printf("Stack Underflow \n");
+    else
+     printf("Top element : %d\n",stack[top]);
+}
+
+void display(){
+    if(top == -1)
+        {printf("Stack Underflow \n");
+        return ;}
+    printf("Stack ");
+    for(int i = top;i>=0;i--)
+        printf("%d ",stack[i]);
+    printf("\n");
+    
+}
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    
+    pop();
+    peek();
+    display();
+
+    return 0;
+}
